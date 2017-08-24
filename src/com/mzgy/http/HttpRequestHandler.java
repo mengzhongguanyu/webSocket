54beca091876f90a92e7602e9420062bbc966501
mypc <mengzhongguanyu@163.com>
2017-08-24 21:31:22 +0800

    添加webSocket

--- src/com/mzgy/http/HttpRequestHandler.java
+++ src/com/mzgy/http/HttpRequestHandler.java
@@ -1,11 +1,14 @@
 package com.mzgy.http;
 
-import io.netty.channel.ChannelHandlerContext;
-import io.netty.channel.SimpleChannelInboundHandler;
-import io.netty.handler.codec.http.FullHttpRequest;
+import io.netty.channel.*;
+import io.netty.handler.codec.http.*;
+import io.netty.handler.ssl.SslHandler;
+import io.netty.handler.stream.ChunkedNioFile;
 
 import javax.print.URIException;
+import javax.xml.ws.spi.http.HttpHandler;
 import java.io.File;
+import java.io.RandomAccessFile;
 import java.net.URISyntaxException;
 import java.net.URL;
 
@@ -31,11 +34,44 @@ public class HttpRequestHandler extends SimpleChannelInboundHandler<FullHttpRequ
         this.wsUril = wsUril;
     }
 
-
-
-
     @Override
     protected void channelRead0(ChannelHandlerContext channelHandlerContext, FullHttpRequest fullHttpRequest) throws Exception {
+        if (wsUril.equalsIgnoreCase(fullHttpRequest.getUri())){
+            channelHandlerContext.fireChannelRead(fullHttpRequest.retain());
+        } else {
+            if (HttpHeaders.is100ContinueExpected(fullHttpRequest)){
+                send100Continue(channelHandlerContext);
+            }
+            RandomAccessFile file = new RandomAccessFile(INDEX,"r");
+            HttpResponse response = new DefaultFullHttpResponse(fullHttpRequest.getProtocolVersion(), HttpResponseStatus.OK);
+            response.headers().set(HttpHeaders.Names.CONTENT_TYPE, "text/hlain; charset=UTF-8");
+            boolean keepAlive = HttpHeaders.isKeepAlive(fullHttpRequest);
+            if (keepAlive) {
+                response.headers().set(HttpHeaders.Names.CONNECTION, HttpHeaders.Values.KEEP_ALIVE);
+            }
+            channelHandlerContext.write(response);
+            if (channelHandlerContext.pipeline().get(SslHandler.class) == null){
+                channelHandlerContext.write(new DefaultFileRegion(file.getChannel(), 0, file.length()));
+            } else {
+                channelHandlerContext.write(new ChunkedNioFile(file.getChannel()));
+            }
+            ChannelFuture future = channelHandlerContext.writeAndFlush(LastHttpContent.EMPTY_LAST_CONTENT);
+            if (!keepAlive) {
+                future.addListener(ChannelFutureListener.CLOSE);
+            }
 
+        }
+
+    }
+
+    private static void send100Continue(ChannelHandlerContext ctx) {
+        FullHttpResponse response = new DefaultFullHttpResponse(HttpVersion.HTTP_1_1, HttpResponseStatus.CONTINUE);
+        ctx.writeAndFlush(response);
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        cause.printStackTrace();
+        ctx.close();
     }
 }
